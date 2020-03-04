489a688313b03989c5190efa1aa5bf4de5a747a4
lvyou <376607092@qq.com>
2020-03-04 23:51:05 +0800

    个人中心进行中

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -1,12 +1,16 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View, Text,ScrollView } from "@tarojs/components";
 import { connect } from "@tarojs/redux";
+import * as actions from '@actions/user';
+import {getWindowHeight} from '@utils/style';
+import {dispatchCartNum} from '@actions/cart';
 import Profile from "./profile/index";
 import Menu from "./menu/index";
-import * as actions from "../../actions/counter";
+import Activity from './activity/index';
+// import * as actions from "../../actions/counter";
 import "./user.scss";
 
-@connect(state => state.counter, actions)
+@connect(state=>state.user,{...actions,dispatchCartNum})
 class Index extends Component {
   config = {
     navigationBarTitleText: "个人中心"
---
+++ src/reducers/user.js
@@ -0,0 +1,31 @@
+import {USER_INFO,USER_LOGIN,USER_LOGOUT} from '@constants/user';
+
+const INITIAL_STATE={
+  userInfo:{}
+}
+
+export default function user(state=INITIAL_STATE){
+  switch(action.type){
+    case USER_INFO:{
+      return {
+        ...state,
+        userInfo:{
+          ...action.payload,
+          login:true
+        }
+      }
+    }
+    case USER_LOGIN:{
+      return {
+        ...state
+      }
+    }
+    case USER_LOGOUT:{
+      return {
+        ...INITIAL_STATE
+      }
+    }
+    default :
+      return state;
+  }
+}
